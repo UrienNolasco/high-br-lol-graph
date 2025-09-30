9b92b5c8cb69b7f706a29d1f323e443ca6e31efe
Urien Nolasco <urien.dragon@gmail.com>
2025-09-30 14:55:35 -0300

    feat: create the sctructure  for the worker and queue

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,9 @@ import { AppConfigModule } from './core/config/config.module';
 import { QueueModule } from './core/queue/queue.module';
 import { DatabaseModule } from './core/database/database.module';
 import { ApiModule } from './modules/api/api.module';
+import { WorkerController } from './modules/worker/worker.controller';
+import { WorkerModule } from './modules/worker/worker.module';
+import { CollectorModule } from './modules/collector/collector.module';
 
 @Module({
   imports: [
@@ -18,7 +21,7 @@ import { ApiModule } from './modules/api/api.module';
     CollectorModule,
     WorkerModule,
   ],
-  controllers: [AppController],
+  controllers: [AppController, WorkerController],
   providers: [AppService],
 })
 export class AppModule {}
