c48c414cfc694f80812573b822dfa5f3f27f07ed
Urien Nolasco <urien.dragon@gmail.com>
2025-10-01 23:38:53 -0300

    fix: update RabbitMQ queue name to use environment variable

--- src/core/queue/queue.module.ts
+++ src/core/queue/queue.module.ts
@@ -15,7 +15,7 @@ import { QueueService } from './queue.service';
             urls: [
               `amqp://${configService.get('RABBITMQ_DEFAULT_USER')}:${configService.get('RABBITMQ_DEFAULT_PASS')}@${configService.get('RABBITMQ_HOST')}`,
             ],
-            queue: 'default_queue',
+            queue: `${configService.get('RABBITMQ_QUEUE')}`,
             queueOptions: {
               durable: true,
             },
