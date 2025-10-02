cc762a23cf4f93dfe5471e635a89170aa45255cb
Urien Nolasco <urien.dragon@gmail.com>
2025-10-02 10:40:48 -0300

    fix: updating rabbit version and add prismamodule on collector.module

--- src/modules/collector/collector.module.ts
+++ src/modules/collector/collector.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { CollectorService } from './collector.service';
 import { RiotModule } from '../../core/riot/riot.module';
 import { QueueModule } from '../../core/queue/queue.module';
+import { PrismaModule } from 'src/core/prisma/prisma.module';
 
 @Module({
-  imports: [RiotModule, QueueModule],
+  imports: [RiotModule, QueueModule, PrismaModule],
   providers: [CollectorService],
   exports: [CollectorService],
 })
