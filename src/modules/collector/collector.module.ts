01cfbc9a4f166d25cba8a72aabb09fe4bd6cb9fe
Urien Nolasco <urien.dragon@gmail.com>
2025-10-02 10:30:12 -0300

    refactor: remove TypeORM module imports from CollectorModule and WorkerModule

--- src/modules/collector/collector.module.ts
+++ src/modules/collector/collector.module.ts
@@ -1,16 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { CollectorService } from './collector.service';
-import { ProcessedMatch } from '../../core/database/entities/processed-match.entity';
 import { RiotModule } from '../../core/riot/riot.module';
 import { QueueModule } from '../../core/queue/queue.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([ProcessedMatch]),
-    RiotModule,
-    QueueModule,
-  ],
+  imports: [RiotModule, QueueModule],
   providers: [CollectorService],
   exports: [CollectorService],
 })
--- src/modules/worker/worker.module.ts
+++ src/modules/worker/worker.module.ts
@@ -1,17 +1,10 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { WorkerService } from './worker.service';
 import { WorkerController } from './worker.controller';
 import { RiotModule } from '../../core/riot/riot.module';
-import { ChampionStats } from '../../core/database/entities/champion-stats.entity';
-import { MatchupStats } from '../../core/database/entities/matchup-stats.entity';
-import { ProcessedMatch } from '../../core/database/entities/processed-match.entity';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([ChampionStats, MatchupStats, ProcessedMatch]),
-    RiotModule,
-  ],
+  imports: [RiotModule],
   controllers: [WorkerController],
   providers: [WorkerService],
   exports: [WorkerService],
